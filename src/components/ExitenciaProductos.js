0757bc0018a5d2f63ec31ea3a62cdc6adfccf151
Fernando Beddings <65370627+Fernando-spec-cmd@users.noreply.github.com>
2022-03-30 23:34:39 -0300

    Add files via upload

---
+++ src/components/ExitenciaProductos.js
@@ -0,0 +1,30 @@
+export const ExistenciaProductos = [
+    {
+        nombre: "Whisky",
+        precio: 25000,
+        stock: 10,
+        id:1,
+        url: "https://ripleycl.imgix.net/http%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2F2021%2F01%2F18161526%2FWhisky-Johnnie-Walker-Red-Label-750-ml.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&bg=FFFFFF&q=60&trimcolor=FFFFFF&trim=color&fit=fillmax&ixlib=js-1.1.0&s=a1a3bcec657a1fdecb5e73f5167a7ac3"
+    },
+    {
+        nombre: "Tequila",
+        precio: 15000,
+        stock: 10,
+        id:2,
+        url: "https://cdn.shopify.com/s/files/1/0617/0070/2429/products/Tequila-Jose-cuervo-reposado750cc_2ba411a5-ddb9-44fa-9780-e5305725c918_1800x1800.png?v=1639404535"
+    },
+    {
+        nombre: "Vodka",
+        precio: 34000,
+        stock: 10,
+        id:3,
+        url: "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/20042152-zkSmUbxb-large.jpg"
+    },
+    {
+        nombre: "Gin",
+        precio: 15000,
+        stock: 10,
+        id:4,
+        url: "https://cdn.shopify.com/s/files/1/0617/0070/2429/products/Gin-Tanqueray750cc_3e8b2985-04ef-4ac8-9da3-6593b9af5c6b_1800x1800.png?v=1639404558"
+    }
+];
