07848eba2506f6f1ea682ff4ee7054098f878b62
Fernando Beddings <65370627+Fernando-spec-cmd@users.noreply.github.com>
2022-03-30 23:30:21 -0300

    Update ItemListContainer.js

--- src/components/containers/ItemListContainer.js
+++ src/components/containers/ItemListContainer.js
@@ -1,5 +1,43 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { ItemList } from "../ItemList";
+import { ExistenciaProductos } from "../ExitenciaProductos";
+import ClipLoader from "react-spinners/ClipLoader";
 
-export const ItemListContainer = ({greetings}) =>{
-    return <h1>{greetings}</h1>
-}
+
+const promesa = new Promise((res) => {
+    setTimeout(() => {
+        res(ExistenciaProductos);
+    }, 1000);
+});
+
+export const ItemListContainer = ({greetings}) => {
+
+    const [productos, setProductos] = useState([]);
+    const [cargar, setCarga] = useState(true);
+
+    useEffect(() => {
+        promesa
+        .then((productos) => {
+            setProductos(productos);
+        })
+        .catch((error) => {
+            console.error("error: ", error);
+        })
+        .finally(() => {
+            setCarga(false);
+        });
+    }, []);
+
+    return (
+        <>
+        <h1>{greetings}</h1>
+        {cargar ? (
+            <span>
+                <ClipLoader color = {"green"} cargar = {cargar} size = {150} />
+            </span>
+        ) : (
+            <ItemList productos={productos} />
+        )}
+        </>
+        );
+    };
